adcb1eaa7bc58ead3271dd62ff1002f94a2c459f
李子奥 <2293485914@qq.com>
2020-07-15 21:16:30 +0800

    fix some bugs

--- ex1.py
+++ ex1.py
@@ -14,7 +14,7 @@ def remove_data_out3sigma(l):
 tp          = {3:4.3, 4:3.18, 5:2.78, 6:2.57,
                 7:2.45, 8:2.37, 9:2.31, 10:1.96}
 
-
+T = 5
 len_string  = np.array([74.05, 73.70, 73.75, 73.35, 73.90])
 time_5T     = np.array([8.82, 8.85, 8.66, 8.78, 8.78])
 len_string  = len_string / 100
@@ -26,7 +26,7 @@ avg_string  = np.mean(len_string)
 avg_time_5T = np.mean(time_5T)
 
 ua_string   = tp[len(len_string)] * calc_sigma(len_string) / np.sqrt(len(len_string))
-ua_time_5T  = tp[len(time_5T)] * calc_sigma(time_5T / len(time_5T)) / np.sqrt(len(time_5T))
+ua_time_5T  = tp[len(time_5T)] * calc_sigma(time_5T / T) / np.sqrt(len(time_5T))
 
 ub_string   = 0.001
 ub_time_5T  = 0.01
@@ -37,11 +37,11 @@ u_time_5T   = np.sqrt(ua_time_5T ** 2   + ub_time_5T ** 2)
 ur_g        = np.sqrt(np.square(u_string / avg_string) + 
                         np.square(2 * u_time_5T / avg_time_5T))
 
-avg_g = 100 * np.pi * np.pi * avg_string / (avg_time_5T ** 2)
+avg_g = 4 * T * T * np.pi * np.pi * avg_string / (avg_time_5T ** 2)
 
 print('Calculated ur_g = {}'.format(ur_g))
 print('Calculated g={}'.format(avg_g))
 print('Estimated g falls in range({}, {})'.format(avg_g*(1-ur_g), avg_g*(1+ur_g)))
-print('Phyphox g : {}'.format(9.68))
+print('Phyphox g : {}'.format(9.63))
 
 
